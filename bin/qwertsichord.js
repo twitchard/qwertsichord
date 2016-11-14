fe1cbb4a2916e520ee7f0d8824ef2c0f906c917c
Richard Marmorstein <richard.marmorstein@livestream.com>
2016-11-13 21:43:18 -0500

    Refactoring into executable module

---
+++ bin/qwertsichord.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const output       = require('../lib/output')
+const xevEmitter   = require('xev-emitter')(process.stdin)
+
+const CONFIGURATION_NAME = process.argv[2] || 'qwerty'
+
+const configuration = require('../lib/configurations/' + CONFIGURATION_NAME)(output, xevEmitter)
+
+
+process.on('SIGTERM', configuration.stop)
+process.on('SIGTERM', process.exit.bind(process, 0))
+process.on('SIGINT', configuration.stop)
+process.on('SIGINT', process.exit.bind(process, 0))
